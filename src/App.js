64822d872b0656a075631fa53f8971fc1c63157a
vovka26 <317110@gmail.com>
2019-02-03 21:24:15 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -9,12 +9,17 @@ class App extends Component {
         fetch(`http://api.open-notify.org/astros.json`)
         .then(res => res.json())
         .then(data => {
+            debugger
             this.setState({people: data.people})
         })
     }
 
     render(){
-        return <div></div>
+        return (
+        <div>
+            {this.state.people.map((person, index) => <div key={index}>{person.name}</div>)}
+        </div>
+        )
     }
 }
 
